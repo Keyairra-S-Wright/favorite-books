bf137edfabcb902909968a9e35e66db75fccefe5
Keyairra Wright <keyairra.wright@gmail.com>
2018-12-10 18:15:25 -0500

    updating resolve methods and adding mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,4 @@
 const graphql = require('graphql');
-//const _ = require('lodash');
 const Book = require('../models/book');
 const Author = require('../models/author');
 
@@ -12,34 +11,16 @@ const {
     GraphQLID
 } = graphql;
 
-//dummy data
-// let books = [
-//     {title: 'Count of Monte Cristo', genre: 'Adventure', id: '1', authorId: '1'},
-//     {title: 'The Bluest Eye', genre: 'Drama', id: '2', authorId: '2'},
-//     {title: 'Oh, the Places You\'ll Go', genre: 'Children\'s', id: '3', authorId: '3'},
-//     {title: 'Three Muskateers', genre: 'Adventure', id: '4', authorId: '1'},
-//     {title: 'Jazz', genre: 'Historical Fiction', id: '5', authorId: '2'},
-//     {title: 'I Know Why the Caged Bird Sings', genre: 'Autobiography', id: '6', authorId: '4'}
-// ];
-
-// let authors = [
-//     {name: 'Alexander Dumas', age: 68, id: '1'},
-//     {name: 'Toni Morrison', age: 87, id: '2'},
-//     {name: 'Dr. Seuss', age: 87, id: '3'},
-//     {name: 'Maya Angelou', age: 86, id: '4'}
-// ];
-
-//Define schema - object types, relationships between them, and entry points into the graph.
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
-    fields: () => ({//in this case, wrapping the fields property inside of a function becasue it always for the code to be run through, top to bottom, but does not execute our fields function until all of our Type variables have been both defined and recognized during run-time
+    fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         age: {type: GraphQLInt},
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                // return _.filter(books, {authorId: parent.id});
+                return Book.find({authorId: parent.id});
             }
         }
     })
@@ -54,8 +35,7 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args){//parent is a necessary parameter when we utlize nested data
-                console.log(parent);
-                // return _.find(authors, {id: parent.authorId}); //look through authors array to find the authorId of the parent book that we are searching for
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -67,29 +47,27 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: { id: {type: GraphQLID}}, //When BookType is queried, arguments are expected to be passed along
-            resolve(parent, args){ //resolve function looks at the data and determines what is needed
-                //code to get data from db  or dummy data
-                // return _.find(books, {id: args.id});
+            resolve(parent, args){
+                return Book.findById(args.id);
             }
         },
         author: {
             type: AuthorType,
             args: { id: {type: GraphQLID}},
             resolve(parent, args){
-                // return _.find(authors, {id: args.id}); //at the moment, with dummy data, this lodash function requires that we look inside of th authors array
-
+                return Author.findById(args.id);
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(){
-                // return books;
+                return Book.find({});//find method with no criteria returns all matches
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(){
-                // return authors;
+                return Author.find({});
             }
         }
     }
